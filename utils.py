58369ed1967cd1ffe7b0f42bb5ea1337b5c82043
gustav-rohss-qred <gustav.rohss@qred.com>
2021-11-30 18:48:43 +0100

    Utility function for [0, 1] visualization scaling

--- utils.py
+++ utils.py
@@ -4,3 +4,9 @@ tfk = tf.keras
 @tf.function
 def preprocess_iv3(x, y):
     return tfk.applications.inception_v3.preprocess_input(x), y
+
+def to_unitrange(x):
+    x = x.copy()
+    x -= x.min()
+    x /= x.max()
+    return x
