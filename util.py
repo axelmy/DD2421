31e88f4d9e1f26b9cb3076314991566dd4a5b69a
SamsTheGreatest <41646769+SamsTheGreatest@users.noreply.github.com>
2021-12-01 14:05:00 +0100

    Update util.py

--- util.py
+++ util.py
@@ -5,55 +5,6 @@ import numpy as np
 import matplotlib
 from tqdm.auto import tqdm
 
-def compute_g(net,x,target_idx=None):
-    net.eval()
-    x.requires_grad = True
-    output = net(x)
-    probabilities = torch.nn.functional.softmax(output, dim=1)
-    if not target_idx: target_idx = torch.argmax(probabilities, 1).item()
-    index = torch.ones((probabilities.size()[0], 1)) * target_idx
-    index = torch.tensor(index, dtype=torch.int64)
-    loss = probabilities.gather(1, index)
-    net.zero_grad()
-    loss.backward(torch.ones_like(loss))
-    x.requires_grad = False
-    grad = x.grad.data
-    return grad
-
-
-
-# def coalition(x):
-    
-    
-# def colalition_approximation(net,x,target_idx=None):
-#     net.eval()
-#     x.requires_grad = False
-#     for 
-#     output = net(x)
-#     probabilities = torch.nn.functional.softmax(output, dim=1)
-    
-    
-#     return grad
-
-class IG(torch.nn.Module):
-    def __init__(self):
-        super().__init__()
-        
-        
-def compute_ig(net,x,target_idx=None,m=3,baseline=None,batch_size=16):
-    assert m>=3
-    net.eval()
-    output = net(x)
-    probabilities = torch.nn.functional.softmax(output, dim=1)
-    if not target_idx: target_idx = torch.argmax(probabilities, 1).item()
-    scaled_inputs = [baseline+alpha*(x-baseline) for alpha in np.linspace(0,1,m)]
-    scaled_inputs = torch.cat(scaled_inputs)
-    grads = torch.cat([compute_g(net, scaled_inputs[i:i+batch_size], target_idx=target_idx) for i in tqdm(range(0,len(scaled_inputs),batch_size),desc='ig')],axis=0)
-    grads = (grads[:-1] + grads[1:]) / 2.0
-    grads_avg = grads.mean(dim=0).reshape(x.shape)
-    ig = (x-baseline)*grads_avg
-    return ig
-
 def normalize(A):
     A-=A.min()
     A/=A.max()
