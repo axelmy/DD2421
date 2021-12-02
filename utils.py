5814e858935057fd90fb366b01ba090b2a313eea
Gustavrohss <gustav.rohss@gmail.com>
2021-12-02 22:58:48 +0100

    Appearently changes something here

--- utils.py
+++ utils.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 tfk = tf.keras
 
 @tf.function
-def preprocess_iv3(x, y):
-    return tfk.applications.inception_v3.preprocess_input(x), y
+def preprocess_iv3(x, *args):
+    return (tfk.applications.inception_v3.preprocess_input(x), *args)
 
 def to_unitrange(x):
     x = x.copy()
