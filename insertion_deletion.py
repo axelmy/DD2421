03df07181a052bc1230bcba2b287ad2590f439f4
Gustavrohss <gustav.rohss@gmail.com>
2021-12-12 21:52:27 +0100

    Metrics

---
+++ insertion_deletion.py
@@ -0,0 +1,92 @@
+import torch
+from torch import nn
+from tqdm import tqdm
+from scipy.ndimage.filters import gaussian_filter
+import numpy as np
+
+# Modified from https://github.com/eclique/RISE
+
+
+HW = 299 * 299 # image area
+n_classes = 1000
+
+def gkern(klen, nsig):
+    """Returns a Gaussian kernel array.
+    Convolution with it results in image blurring."""
+    # create nxn zeros
+    inp = np.zeros((klen, klen))
+    # set element at the middle to one, a dirac delta
+    inp[klen//2, klen//2] = 1
+    # gaussian-smooth the dirac, resulting in a gaussian filter mask
+    k = gaussian_filter(inp, nsig)
+    kern = np.zeros((3, 3, klen, klen))
+    kern[0, 0] = k
+    kern[1, 1] = k
+    kern[2, 2] = k
+    return torch.from_numpy(kern.astype('float32'))
+
+def auc(arr):
+    """Returns normalized Area Under Curve of the array."""
+    return (arr.sum() - arr[0] / 2 - arr[-1] / 2) / (arr.shape[0] - 1)
+
+class CausalMetric():
+
+    def __init__(self, model, mode, step, substrate_fn):
+        r"""Create deletion/insertion metric instance.
+        Args:
+            model (nn.Module): Black-box model being explained.
+            mode (str): 'del' or 'ins'.
+            step (int): number of pixels modified per one iteration.
+            substrate_fn (func): a mapping from old pixels to new pixels.
+        """
+        assert mode in ['del', 'ins']
+        self.model = model
+        self.mode = mode
+        self.step = step
+        self.substrate_fn = substrate_fn
+
+    def single_run(self, img_tensor, explanation, verbose=0, save_to=None):
+        r"""Run metric on one image-saliency pair.
+        Args:
+            img_tensor (Tensor): normalized image tensor.
+            explanation (np.ndarray): saliency map.
+            verbose (int): in [0, 1, 2].
+                0 - return list of scores.
+                1 - also plot final step.
+                2 - also plot every step and print 2 top classes.
+            save_to (str): directory to save every step plots to.
+        Return:
+            scores (nd.array): Array containing scores at every step.
+        """
+        pred = torch.from_numpy(self.model(img_tensor).numpy())
+        top, c = torch.max(pred, 1)
+        c = c.cpu().numpy()[0]
+        n_steps = (HW + self.step - 1) // self.step
+
+        img_tensor = torch.from_numpy(img_tensor.numpy().transpose((0, 3, 1, 2)))
+        
+        if self.mode == 'del':
+            # title = 'Deletion game'
+            # ylabel = 'Pixels deleted'
+            start = img_tensor.clone()
+            finish = self.substrate_fn(img_tensor)
+        elif self.mode == 'ins':
+            # title = 'Insertion game'
+            # ylabel = 'Pixels inserted'
+            start = self.substrate_fn(img_tensor)
+            finish = img_tensor.clone()
+
+        scores = np.empty(n_steps + 1)
+        # Coordinates of pixels in order of decreasing saliency
+        salient_order = np.flip(np.argsort(explanation.reshape(-1, HW), axis=1), axis=-1)
+        for i in range(n_steps+1):
+            pred = self.model(start.numpy().transpose(0, 2, 3, 1))
+            pred = torch.from_numpy(pred.numpy())
+            pr, cl = torch.topk(pred, 2)
+            
+            scores[i] = pred[0, c]
+            
+            if i < n_steps:
+                coords = salient_order[:, self.step * i:self.step * (i + 1)]
+                start.cpu().numpy().reshape(1, 3, HW)[0, :, coords] = finish.cpu().numpy().reshape(1, 3, HW)[0, :, coords]
+        return auc(scores)
